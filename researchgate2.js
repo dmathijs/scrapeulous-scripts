1e3b06bbc96b0c17639cadc2375b1192bfb9abbc
Diederik Mathijs <diederik.mathijs@delaware.pro>
2020-06-05 11:16:34 +0200

    Add researchgate script

---
+++ researchgate2.js
@@ -0,0 +1,62 @@
+/**
+ * @author Diederik Mathijs
+ * @version 1.0
+ * @last_modified May 2020
+ * @website: diederikmathijs.be
+ *
+ * Returns author and some meta information
+ *
+ * Supported options:
+ *
+ * @param url: url of the researchgate publications
+ */
+class ResearchGate extends BrowserWorker {
+    async crawl(url) {
+      await this.page.goto(url);
+      await this.page.waitForSelector('.nova-e-text');
+      await this.page.waitFor(200);
+
+      return await this.page.evaluate(() => {
+          let loadMoreButton = document.querySelector('.show-more-less-authors__button')
+          if(loadMoreButton){
+              loadMoreButton.click();
+          }
+          // ajax load
+          this.page.waitFor(100);
+
+          const data = [];
+          let authors = document.querySelectorAll('.nova-v-person-list-item__align-content');
+
+          authors.forEach((el) => {
+            let obj = {
+                title:null,
+                link:null,
+                affiliation:null
+            }
+
+            let authorTitle = el.querySelector('.nova-v-person-list-item__title > a')
+            if(authorTitle){
+                obj.title = authorTitle.innerText;
+                obj.link = authorTitle.getAttribute('href');
+            }
+
+            let metas = el.querySelectorAll('.nova-v-person-list-item__meta-item > a')
+            if(metas){
+                metas.forEach((meta) => {
+                    if(meta.querySelector('svg')){
+                        // This is the rating, can be changed..
+                    }else{
+                        obj.affiliation = meta.innerText;
+                    }
+                })
+            }
+
+            if(obj.title != null){
+                data.push(obj)
+            }
+            
+            return data;
+          });
+      })
+    }
+}
